ad6fc8392c6b3988ebb8ceb83a151bf3b6083d7a
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2024-01-03 16:46:02 -0300

    muita coisa
    Aside de amigos

--- frontEnd/src/components/aplicativo/common/aside/Aside.jsx
+++ frontEnd/src/components/aplicativo/common/aside/Aside.jsx
@@ -1,8 +1,52 @@
 import styles from "./Aside.module.scss"
 
 function Aside(props) {
-
   const taDentroDoMenu = props.taDentroDoMenu
+
+  const newAmigo = () => {
+    const containerDeAmigos = document.querySelector('.containerDeAmigos')
+
+    const cardsAmigos = document.createElement('div')
+    cardsAmigos.className = styles.cardsAmigos
+    
+    const nome = document.createElement('p')
+    const nomeDoAmigo = prompt('Nome do novo amigo: ')
+    nome.innerHTML = nomeDoAmigo ? nomeDoAmigo : 'Amigo' 
+
+    const informationsAmigo = document.createElement('div')
+    informationsAmigo.className = styles.informationsAmigo
+
+    const spanOffOn = document.createElement('span')
+    spanOffOn.className = styles.spanOffOn
+
+    const iconeLapis = document.createElement('i')
+    iconeLapis.className = "bi bi-pencil-fill"
+    
+    const spanNotification = document.createElement('span')
+    spanNotification.innerHTML = 0
+
+    informationsAmigo.appendChild(spanOffOn)
+    informationsAmigo.appendChild(iconeLapis)
+    informationsAmigo.appendChild(spanNotification)
+
+    cardsAmigos.appendChild(nome)
+    cardsAmigos.appendChild(informationsAmigo)
+
+    containerDeAmigos.appendChild(cardsAmigos)
+  }
+
+
+
+
+  const mudarNome = (event) => {
+    const elemento = event.target.parentNode.parentNode.firstElementChild
+    const newNome = prompt('nome: ')
+
+    newNome ? elemento.innerHTML = newNome : null
+    console.log(elemento)
+  }
+
+
 
   return (
     <div className={styles.aside} id={taDentroDoMenu && [styles.dentroDoMenu]}>
@@ -13,70 +57,27 @@ function Aside(props) {
         <input type="text" className={styles.inputPrucurarPorAmigo} />
       </div>
 
-      <button className={styles.buttonAdicionarNovoAmigo}>
+      <button className={styles.buttonAdicionarNovoAmigo} onClick={newAmigo}>
         <i class="bi bi-plus-lg"></i> New Amigo 
       </button>
 
-      <div className={styles.cardsAmigos}>
-        Ramon
-        <div className={styles.informationsAmigo}>
-          <span className={styles.spanOffOn}></span>
-          <i class="bi bi-pencil-fill"></i>
-          <span>4</span>
+      <div className='containerDeAmigos'>
+        <div className={styles.cardsAmigos}>
+          <p>Ramon</p>
+          <div className={styles.informationsAmigo}>
+            <span className={styles.spanOffOn}></span>
+            <i class="bi bi-pencil-fill" onClick={mudarNome}></i>
+            <span>4</span>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.cardsAmigos}>
-        Davi
-        <div className={styles.informationsAmigo}>
-          <span className={styles.spanOffOn}></span>
-          <i class="bi bi-pencil-fill"></i>
-          <span>1</span>
-        </div>
-      </div>
-
-      <div className={styles.cardsAmigos}>
-        Maria
-        <div className={styles.informationsAmigo}>
-          <span className={styles.spanOffOn}></span>
-          <i class="bi bi-pencil-fill"></i>
-          <span>1</span>
-        </div>
-      </div>
-
-      <div className={styles.cardsAmigos}>
-        Lucas
-        <div  className={styles.informationsAmigo}>
-          <span className={styles.spanOffOn}></span>
-          <i class="bi bi-pencil-fill"></i>
-          <span>0</span>
-        </div>
-      </div>
-
-      <div className={styles.cardsAmigos}>
-        Ray
-        <div className={styles.informationsAmigo}>
-          <span className={styles.spanOffOn}></span>
-          <i class="bi bi-pencil-fill"></i>
-          <span>0</span>
-        </div>
-      </div>
-      
-      <div className={styles.cardsAmigos}>
-        Ray
-        <div className={styles.informationsAmigo}>
-          <span className={styles.spanOffOn}></span>
-          <i class="bi bi-pencil-fill"></i>
-          <span>0</span>
-        </div>
-      </div>
-
-      <div className={styles.cardsAmigos}>
-        LOREN IP ...
-        <div className={styles.informationsAmigo}>
-          <span className={styles.spanOffOn}></span>
-          <i class="bi bi-pencil-fill"></i>
-          <span>0</span>
+        
+        <div className={styles.cardsAmigos}>
+          LOREN IP ...
+          <div className={styles.informationsAmigo}>
+            <span className={styles.spanOffOn}></span>
+            <i class="bi bi-pencil-fill"></i>
+            <span>0</span>
+          </div>
         </div>
       </div>
     </div>
