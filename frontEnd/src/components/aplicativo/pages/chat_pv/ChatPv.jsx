96061c8dc536c4b4cdc0fe63ef84fb02bbe241a0
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2024-02-10 10:58:36 -0300

    Esboço da pagina de Chat

--- frontEnd/src/components/aplicativo/pages/chat_pv/ChatPv.jsx
+++ frontEnd/src/components/aplicativo/pages/chat_pv/ChatPv.jsx
@@ -1,16 +1,49 @@
 import styles from "./ChatPv.module.scss"
 
-export default function ChatPv() {
+export default function ChatPv(props) {
+    const tema = props.tema
+
+
     return (
-        <div className={styles.containerChat}>
+        <div className={styles.containerChat} id={tema == 'Escuro' ? styles.temaDark : null}>
             <h1 className={styles.h1}>Chat</h1>
             
-            <div className={styles.my}>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sed eveniet et autem exercitationem id iusto, velit hic laborum sapiente fugiat ipsum repudiandae. Atque odio dolore quae accusamus debitis esse?
-            </div>
+            <div className={styles.chat}>
+                <div className={styles.msg} id={styles.voce}>
+                    <h4>You</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sed eveniet et autem exercitationem id iusto, velit hic laborum sapiente fugiat ipsum repudiandae. Atque odio dolore quae accusamus debitis esse?</p>
+                </div>
+                <div className={styles.msg} id={styles.amigo}>
+                    <h4>Ramon</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sed eveniet et autem exercitationem id iusto, velit hic laborum sapiente fugiat ipsum repudiandae. Atque odio dolore quae accusamus debitis esse?</p>
+                </div>
 
-            <div className={styles.you}>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sed eveniet et autem exercitationem id iusto, velit hic laborum sapiente fugiat ipsum repudiandae. Atque odio dolore quae accusamus debitis esse?
+
+
+
+
+
+
+
+
+                <div className={styles.msg} id={styles.voce}>
+                    <h4>You</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit?</p>
+                </div>
+                <div className={styles.msg} id={styles.amigo}>
+                    <h4>Ramon</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sed eveniet et autem exercitationem id?</p>
+                </div><div className={styles.msg} id={styles.voce}>
+                    <h4>You</h4>
+                    <p>Lorem</p>
+                </div>
+                <div className={styles.msg} id={styles.amigo}>
+                    <h4>Ramon</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sed eveniet et autem exercitationem id iusto,</p>
+                </div><div className={styles.msg} id={styles.voce}>
+                    <h4>You</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi sed eveniet et autem exercitationem id iusto, velit hic laborum sapiente fugiat ipsum repudiandae. Atque odio dolore quae accusamus debitis esse?</p>
+                </div>
             </div>
         </div>
     )
